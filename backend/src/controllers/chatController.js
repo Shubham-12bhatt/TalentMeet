dee1ddc91df4c149e319d9fe4d7aca325f94ca73
Shubham-12bhatt <bhattshubham466@gmail.com>
2026-03-24 22:34:20 +0530

    server side auth implemented

---
+++ backend/src/controllers/chatController.js
@@ -0,0 +1,19 @@
+import { chatClient } from "../lib/stream.js";
+
+
+export async function getStreamToken(req, res) {
+  try {
+    const token = await chatClient.createToken(req.user.clerkId);
+    res.status(200).json({
+      token,
+      userId: req.user.clerkId,
+      userName: req.user.name,
+      userImage: req.user.image
+    })
+        
+    } catch (error) {
+    res.status(500).json({
+          message:"error in getStreamToken"
+        })
+    }
+}
