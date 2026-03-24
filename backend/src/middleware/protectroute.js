6fdcfe41b01932d530a35effe6e609ca9c3c2f2c
Shubham-12bhatt <bhattshubham466@gmail.com>
2026-03-24 22:35:44 +0530

    minor fixes

--- backend/src/middleware/protectroute.js
+++ backend/src/middleware/protectroute.js
@@ -7,18 +7,18 @@ export const protectRoute = [
       const clerkId = req.auth().userId;
       if (!clerkId) {
         return res.status(401).json({
-          msg:"unauthorized invalid token"
+          message:"unauthorized invalid token"
         })
       }
       const user = await User.findOne({ clerkId })
       if (!user) {
-        return res.status(404).json({ msg: "User not found" });
+        return res.status(404).json({ message: "User not found" });
       }
       req.user = user
       next();
     } catch (error) {
       console.log("error in protected route middleware", error);
-      res.status(500).json({msg:"internal server error"})
+      res.status(500).json({message:"internal server error"})
     }
   }
 ]
