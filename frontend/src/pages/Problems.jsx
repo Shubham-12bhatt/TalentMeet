b2dcb4257bb7f947d1c2f9a5d7679bc3ba31bc07
Shubham-12bhatt <bhattshubham466@gmail.com>
2026-04-04 23:32:34 +0530

    minor

--- frontend/src/pages/Problems.jsx
+++ frontend/src/pages/Problems.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Problems = () => {
   return (
-    <div>Problems</div>
+    <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-20 pb-16'>
+      <div>
+
+      </div>
+    </div>
   )
 }
 
